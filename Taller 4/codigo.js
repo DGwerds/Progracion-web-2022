740e2d5660f0e84134dc6d03ae21b41565584e90
Werds <66983078+DGwerds@users.noreply.github.com>
2022-11-23 17:53:02 -0500

    sin terminar

---
+++ Taller 4/codigo.js
@@ -0,0 +1,13 @@
+
+document.getElementById("nombre")[0].addEventListener('change', doThing);
+
+function doThing(){
+   alert('Horray! Someone wrote "' + this.value + '"!');
+}
+
+function myFunc(){
+    alert("You Clicked me");
+ }
+
+ https://stackoverflow.com/questions/26946235/pure-javascript-listen-to-input-value-change
+ live preview
