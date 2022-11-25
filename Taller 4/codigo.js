5aba4e7e57c4657f71638aa43ab61c1b823c1c4f
Werds <santiagoandresxbox@gmail.com>
2022-11-25 17:42:08 -0500

    Simplificación de código
    Tabulacion cambiada a 2 espaciados porque 4 era mucho.

--- Taller 4/codigo.js
+++ Taller 4/codigo.js
@@ -1,14 +1,15 @@
-
-document.getElementById("nombre").addEventListener("input", validar);
-document.getElementById("apellido").addEventListener("input", validar);
-document.getElementById("cedula").addEventListener("input", validar);
-document.getElementById("email").addEventListener("input", validar);
+// Para todos los elementos de inputs basicos se ejecuta la validacion basica
+document.getElementsByName("inputB").forEach(obj => obj.addEventListener("input", validar))
 
 error = true;
 
+/* Validacion basica
+   Resalta en rojo el texto ingresado en el input si este no cumple con la cantidad minima y 
+   maxima de caracteres requeridos
+   Se configura desde las propiedades del input (propiedad min y max)                     */
 function validar() {
    var tamano = this.value.length;
-   if (tamano >= this.getAttribute("maxlength") || tamano < this.getAttribute("minlength")) {
+   if (tamano >= this.getAttribute("max") || tamano < this.getAttribute("min")) {
       this.style.color = "red";
       error = true;
    } else {
@@ -17,10 +18,11 @@ function validar() {
    }
 }
 
-function estado(){
-   if (error){
-      document.getElementById("lab").style.visibility = "visible";
-   }else{
-      document.getElementById("lab").style.visibility = "hidden";
+function estado() {
+   var mensaje = document.getElementById("Mensaje_error")
+   if (error) {
+      mensaje.style.visibility = "visible";
+   } else {
+      mensaje.style.visibility = "hidden";
    }
 }
