d33f8df4c7645af473c935be292431e68b29d39b
Werds <santiagoandresxbox@gmail.com>
2022-11-25 18:47:01 -0500

    Validaciones
    Solo falta el de la contraseña

--- Taller 4/codigo.js
+++ Taller 4/codigo.js
@@ -1,25 +1,35 @@
 // Para todos los elementos de inputs basicos se ejecuta la validacion basica
-document.getElementsByName("inputB").forEach(obj => obj.addEventListener("input", validar))
+document.getElementsByName("inputB").forEach(obj => obj.addEventListener("input", validar_tamaño))
 
-error = true;
+document.getElementById("direccion").addEventListener("input", validar_direccion)
 
-/* Validacion basica
-   Resalta en rojo el texto ingresado en el input si este no cumple con la cantidad minima y 
-   maxima de caracteres requeridos
-   Se configura desde las propiedades del input (propiedad min y max)                     */
-function validar() {
-   var tamano = this.value.length;
-   if (tamano >= this.getAttribute("max") || tamano < this.getAttribute("min")) {
-      this.style.color = "red";
+let error = true;
+
+function validar_tamaño() {
+   let tamano = this.value.length;
+   let condicion = tamano >= this.getAttribute("max") || tamano < this.getAttribute("min")
+   cambiar_color(this, condicion)
+}
+
+function validar_direccion() {
+   let string = this.value;
+   let expReg = /^cll|cra|av|anv|trans/
+   cambiar_color(this, !expReg.test(string))
+}
+
+function cambiar_color(objeto, condicion){
+   console.log(condicion)
+   if (condicion) {
+      objeto.style.color = "red";
       error = true;
    } else {
       error = false;
-      this.style.color = "black";
+      objeto.style.color = "black";
    }
 }
 
 function estado() {
-   var mensaje = document.getElementById("Mensaje_error")
+   let mensaje = document.getElementById("Mensaje_error")
    if (error) {
       mensaje.style.visibility = "visible";
    } else {
