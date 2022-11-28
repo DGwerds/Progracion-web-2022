14ce7b862dc4edded3d287e94be2e3789f1bbb2e
Werds <66983078+DGwerds@users.noreply.github.com>
2022-11-28 09:40:09 -0500

    Cambios

--- Taller 4/codigo.js
+++ Taller 4/codigo.js
@@ -1,12 +1,12 @@
-// Para todos los elementos de inputs basicos se ejecuta la validacion basica
 let elementos = document.getElementsByName("inputB")
 elementos.forEach(obj => obj.addEventListener("input", validar_tamaño))
 
 document.getElementById("direccion").addEventListener("input", validar_direccion)
 
-// falta contrasenia
-
 let error = document.getElementById("Mensaje_error");
+
+contraseña: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#%/&])(?!\s)[a-zA-Z0-9#%/&]{15,20}$/,
+email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
 
 function validar_tamaño() {
    let tamano = this.value.length;
@@ -14,9 +14,9 @@ function validar_tamaño() {
    let condicion = tamano < rango[0] || tamano > rango[1]
    let mensaje = "El campo " + this.id + " "
    if (rango[0] != 1) {
-      mensaje+="debe contener entre "+rango[0]+" y "+rango[1] + " caracteres"
+      mensaje += "debe contener entre " + rango[0] + " y " + rango[1] + " caracteres"
    } else {
-      mensaje+="no puede quedar vacio y no debe sobrepasar los "+rango[1]+" caracteres"
+      mensaje += "no puede quedar vacio y no debe sobrepasar los "+rango[1]+" caracteres"
    }
    cambiar_color(this, condicion, mensaje)
 }
@@ -25,15 +25,16 @@ function validar_direccion() {
    let condicion = !/^cll|cra|av|anv|trans/.test(this.value)
    const mensaje = "El campo dirección debe empezar por las siguientes palabras.\
       cll, cra, av, anv, trans,"
+   if (this.value == "") { condicion = false }
    cambiar_color(this, condicion, mensaje)
 }
 
 function cambiar_color(objeto, condicion, mensaje) {
    if (condicion) {
-      objeto.style.color = "red";
+      objeto.style.borderBottomColor = "red";
       error.innerHTML = mensaje
    } else {
-      objeto.style.color = "black";
+      objeto.style.borderBottomColor = "green";
       error.innerHTML = ""
    }
 }
@@ -42,6 +43,7 @@ function validacion_final() {
    let mensaje = ""
    elementos.forEach(function (el) {
       if (el.value == "" || el.style.color == "red") {
+         cambiar_color(el, true)
          mensaje += "Error en la casilla " + el.id + " <br/>"
       }
    });
